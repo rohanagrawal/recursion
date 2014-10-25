c5422d1af84c0494b06a1c1b34d1db74d4c12284
Rohan Agrawal <rohankagrawal@gmail.com>
2014-10-25 01:31:21 -0700

    added recursion for arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-	return obj.toString();
+	var finalStr = '';
+	if (obj !== null) {
+		if (typeof obj === 'string') {
+			return '"' + obj + '"';
+		}
+		else if (Array.isArray(obj)) {
+			var stringifyArray = function(param) {
+				var paramStr = '[';
+				if (param.length) {
+					for (var i=0; i < param.length; i++) {
+						if (typeof param[i] === 'string') {
+							paramStr += '"' + param[i] + '"';
+						}
+						else if (Array.isArray(param[i])) {
+							paramStr += stringifyArray(param[i]);
+						}
+						else {
+							paramStr += param[i].toString();
+						}
+						if (i !== (param.length-1)) {
+							paramStr += ',';
+						}
+					}
+				}
+				paramStr += ']';
+				return paramStr;
+			}
+			finalStr += stringifyArray(obj);
+			return finalStr;
+		}
+		else {
+			return obj.toString();
+		}
+	}
+	else {
+		return 'null';
+	}
 };
